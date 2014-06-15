06b7001dc373d2eb24a51b2e15087cf6985ca9d6
Rachel Self <rachel.m.self@gmail.com>
2014-06-15 13:55:05 -0500

    google auth working

--- app/lib/init-routes.js
+++ app/lib/init-routes.js
@@ -70,6 +70,22 @@ function load(app, fn){
 		}));
 
 
+  // =====================================
+	// GOOGLE ROUTES =======================
+	// =====================================
+
+  // send to google to do the authentication
+	// profile gets us their basic information including their name
+	// email gets their emails
+    app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+    // the callback after google has authenticated the user
+    app.get('/auth/google/callback',
+            passport.authenticate('google', {
+                    successRedirect : '/profile',
+                    failureRedirect : '/'
+            }));
+
 
   console.log('Routes Loaded');
   fn();
