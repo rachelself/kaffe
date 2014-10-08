81c25420c68ee406781d5e8a8b788467f53091f9
Rachel Self <rachel.m.self@gmail.com>
2014-10-08 18:48:51 -0500

    Clean up base.js

--- app/models/base.js
+++ app/models/base.js
@@ -5,30 +5,20 @@
 var Mongo = require('mongodb');
 var _ = require('lodash');
 
-
 class Base{
   static findById(id, collection, model, fn){
-    // console.log('=== made it to find by id - BASE =====');
-    // console.log(id);
     if(typeof id === 'string'){
       if(id.length !== 24){fn(null, null); return;}
       id = Mongo.ObjectID(id);
-      // console.log(id);
     }
     if(!(id instanceof Mongo.ObjectID)){fn(null, null); return;}
 
     collection.findOne({_id:id}, (e,obj)=>{
-      // console.log('==== looking for on obj... ====');
       if(obj){
-        // console.log('==== found an obj... ====');
-        // console.log(obj);
         obj = _.create(model.prototype, obj);
-        // console.log('==== changed obj prototype ====');
-        // console.log(obj);
         fn(null, obj);
         return;
       }else{
-        // console.log('==== did not find an obj ====');
         fn(null, null);
         return;
       }
@@ -41,8 +31,6 @@ class Base{
       fn(records);
     });
   }
-
-
 }
 
 module.exports = Base;
